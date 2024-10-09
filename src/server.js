362ef79fd54f6ad82814dfcdaed514e5d244e4b1
Martin Sandström Rimsbo DEV <developer@mapletone.com>
2024-10-09 17:17:03 +0200

    update

--- src/server.js
+++ src/server.js
@@ -39,10 +39,10 @@ function modifySDPForMultichannel(sdp, format) {
     let channelMapping = '';
 
     if (format === '5.1') {
-        audioFormat = 'opus/48000/6';
+        audioFormat = 'multiopus/48000/6';
         channelMapping = 'channel_mapping=0,1,4,5,2,3;num_streams=4;coupled_streams=2';
     } else if (format === '7.1') {
-        audioFormat = 'opus/48000/8';
+        audioFormat = 'multiopus/48000/8';
         channelMapping = 'channel_mapping=0,6,1,2,3,4,5,7;num_streams=5;coupled_streams=3';
     } else {
         // Default to stereo
@@ -50,23 +50,19 @@ function modifySDPForMultichannel(sdp, format) {
         channelMapping = 'stereo=1; sprop-stereo=1;';
     }
 
-    // Make regex case-insensitive and allow for slight formatting variations
-    const stereoRegex = /opus\/48000\/2/i;
-    const stereoParamsRegex = /stereo=1;?\s*/gi;
-    const spropStereoRegex = /sprop-stereo=1;?\s*/gi;
-
     // Remove any occurrences of 'stereo=1' and 'sprop-stereo=1'
-    sdp.sdp = sdp.sdp.replace(stereoParamsRegex, '');
-    sdp.sdp = sdp.sdp.replace(spropStereoRegex, '');
+    sdp.sdp = sdp.sdp.replace(/stereo=1;?\s*/g, '');
+    sdp.sdp = sdp.sdp.replace(/sprop-stereo=1;?\s*/g, '');
 
     // Replace default opus format with the desired audio format
-    sdp.sdp = sdp.sdp.replace(stereoRegex, audioFormat);
+    sdp.sdp = sdp.sdp.replace('opus/48000/2', audioFormat);
     
-    // Add the appropriate channel mapping to the SDP
+    // Add the appropriate channel mapping and correct useinbandfec
     sdp.sdp = sdp.sdp.replace('useinbandfec=1', `useinbandfec=1;${channelMapping}`);
 
     return sdp;
 }
+
 
 
 // Startup
